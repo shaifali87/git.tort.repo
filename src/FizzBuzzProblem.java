39ca47c7eb55f5f7da2f8fe7c12558a9e07aed43
Shaifali <shaifalisingh123@gmail.com>
2019-08-19 23:09:43 -0400

    2nd commit

--- src/FizzBuzzProblem.java
+++ src/FizzBuzzProblem.java
@@ -2,6 +2,7 @@ public class FizzBuzzProblem {
 
 	public static void main(String[] args) {
 		{
+			System.out.println("Hi there !!!");
 			int n = 100;
 			for (int i = 1; i <= n; i++) {
 				if (i % 15 == 0) {
